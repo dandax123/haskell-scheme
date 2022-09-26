57ea69e7dd9b4331cf23d396c732c5270a42ad08
dandax123 <dandaoluks@gmail.com>
2022-09-26 09:31:22 +0300

    feat: simple eval

--- app/Main.hs
+++ app/Main.hs
@@ -3,6 +3,9 @@ module Main where
 import Control.Monad (liftM)
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Text.Parsec (sepBy)
+import GHC.Float (fromRat'')
+import GHC.Base (absentErr)
 
 data LispVal
   = Atom String
@@ -12,8 +15,9 @@ data LispVal
   | String String
   | Bool Bool
   | Float Float
-  deriving (Show)
 
+
+instance Show LispVal where show = showVal
 --  understoodf
 parseString :: Parser LispVal
 parseString = do
@@ -45,7 +49,29 @@ parseNumber :: Parser LispVal
 parseNumber = fmap (Number . read) (many1 digit)
 
 parseExpr :: Parser LispVal
-parseExpr = parseAtom <|> parseString <|> try parseFloat <|> parseNumber
+parseExpr = parseAtom <|> parseString <|> try parseFloat <|> parseNumber <|> parseQuoted <|> 
+  do 
+    char '('
+    x <- try parseList <|> parseDottedList
+    char ')'
+    return x
+
+parseList ::  Parser LispVal
+parseList = liftM List $ sepBy parseExpr spaces
+
+parseQuoted:: Parser LispVal
+parseQuoted  = do
+  char '\''
+  x <- parseExpr
+  return $ List [Atom "quote", x]
+
+parseDottedList :: Parser LispVal
+parseDottedList  = do
+  head <- endBy parseExpr spaces
+  tail <- char '.' >> spaces >> parseExpr
+  return $ DottedList  head tail
+
+
 
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
@@ -53,12 +79,35 @@ symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 spaces :: Parser ()
 spaces = skipMany1 space
 
-readExpr :: String -> String
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of
-  Left err -> "No match: " ++ show err
-  Right val -> "Found value: " ++ show val
+  Left err -> String $ "No match: " ++ show err
+  Right val -> val
+
+
+showVal :: LispVal -> String
+showVal (String c) =  "\"" ++ c ++ "\""
+showVal (Atom a) = a
+showVal (Number a) = show a
+showVal (Bool True)  = "#t"
+showVal (Bool False)  = "#f"
+
+showVal (List contents) = "(" ++ unwordsList contents ++ ")"
+showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tail ++ ")"
+
+
+unwordsList :: [LispVal] -> String
+unwordsList = unwords . map showVal
+
+
+
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
 
 main :: IO ()
 main = do
   (expr : _) <- getArgs
-  putStrLn (readExpr expr)
+  print $ ( eval . readExpr) expr
